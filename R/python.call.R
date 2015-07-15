6ba683e503085ce8c0e2a22dbf19320faadf0b56
Alexander Bessonov <nonsleepr@gmail.com>
2015-07-15 14:56:56 -0400

    Get Object functionality to use Python object from R

--- R/python.call.R
+++ R/python.call.R
@@ -6,12 +6,16 @@
 #' @examples
 #' python.call("str", 100.5)
 #' @export
-python.call <- function(func, ...) {
-  args <- jsonlite::toJSON(list(...))
-  code <- sprintf("pyr._ = json.dumps(%s(*pyr.str_to_args('%s'), **pyr.str_to_kwargs('%s')))", func, args, args)
+python.call <- function(func, ..., .saveTo = "pyr._", .getResults = TRUE) {
+  args <- list(...)
+  args <- jsonlite::toJSON(args)
+  code <- sprintf("%s = %s(*pyr.str_to_args('%s'), **pyr.str_to_kwargs('%s'))", .saveTo, func, args, args)
   .python.exec(code)
-  ret <- python.get("pyr._")
-  if(length(ret) && ret != "") {
-    jsonlite::fromJSON(ret)
+  if (.getResults) {
+    .python.exec(sprintf("pyr._ = json.dumps(%s)", .saveTo))
+    ret <- python.get("pyr._")
+    if(length(ret) && ret != "") {
+      jsonlite::fromJSON(ret)
+    }
   }
 }
